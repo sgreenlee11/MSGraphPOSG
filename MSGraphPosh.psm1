e4ca90bbe1e3fc638c3be008dca1042cd19339b7
sgreenlee11 <sean.greenlee@outlook.com>
2019-03-19 08:31:40 -0500

    Initial Addition of Add-RESTGuest function

--- MSGraphPosh.psm1
+++ MSGraphPosh.psm1
@@ -3,7 +3,6 @@ function Get-AADToken
 	param(
 		[String[]]$Scopes,
 		$ClientID,
-		#$MSALPath,
 		$AppName,
 		$UserName,
 		$Credential,
@@ -12,8 +11,7 @@ function Get-AADToken
 		$TenantID,
 		$Resource
 	)
-	#Load MSAL DLL
-	#Add-Type -Path $msalpath
+
 	#Use Organizations endpoint when sending credentials as part of request, otherwise common
 	If($username -or $Credential)
 	{
@@ -69,7 +67,97 @@ function Get-AADToken
 		$authresult = $clientapp.AcquireTokenAsync($appscopes)
 	}
 	
-	return $authresult.Result
+	return $authresult
 
 	
+}
+
+function Get-RESTMailContacts
+{
+    param(
+        $Authentication,
+        $Resource,
+        $Username
+    )
+    
+    #Create array for output
+    $contactarray = New-Object System.Collections.ArrayList
+
+    #Grab first 10 contacts
+
+    if($username)
+    {
+
+    }
+    else
+    {
+    $contacturl = "https://graph.microsoft.com/v1.0/me/contacts"
+    }
+    $allcontacts = Invoke-RestMethod -Uri $contacturl -Method GET -Headers @{Authorization = $Authentication.result.CreateAuthorizationHeader()}
+
+    #Loop through returned contacts and 
+    foreach($c in $allcontacts.value)
+    {
+        $conobj = New-Object psobject
+        $conobj | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $c.DisplayName
+        $conobj | Add-Member -MemberType NoteProperty -Name "Title" -Value $c.jobTitle
+        $conobj | Add-Member -MemberType NoteProperty -Name "Manager" -Value $c.manager
+        $conobj | Add-Member -MemberType NoteProperty -Name "Address" -Value ($c.emailaddresses | Select-Object address).address
+        [void]$contactarray.Add($conobj)
+    }
+
+    #Check for more than 10 contacts
+    if($allcontacts.'@odata.nextLink' -ne $null)
+    {
+        $morecontacts = $true
+        $nextlink = $allcontacts.'@odata.nextLink'
+    }
+    while($morecontacts -eq $true)
+    {
+        $nextcontacts = Invoke-RestMethod -Uri $nextlink -Method GET -Headers @{Authorization = $Authentication.result.CreateAuthorizationHeader()}
+
+        #Loop through returned contacts and 
+        foreach($c in $nextcontacts.value)
+        {
+            $conobj = New-Object psobject
+            $conobj | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $c.DisplayName
+            $conobj | Add-Member -MemberType NoteProperty -Name "Title" -Value $c.jobTitle
+            $conobj | Add-Member -MemberType NoteProperty -Name "Manager" -Value $c.manager
+            $conobj | Add-Member -MemberType NoteProperty -Name "Address" -Value ($c.emailaddresses | Select-Object address).address
+            [void]$contactarray.Add($conobj)
+        }
+        if($nextcontacts.'@odata.nextLink' -eq $null)
+        {
+            $morecontacts = $false
+        }
+        else {
+            $nextlink = $nextcontacts.'@odata.nextLink' 
+        }
+    }
+
+
+    return $contactarray
+}
+
+function Add-RESTGuest
+{
+    param(
+        $Authentication,
+        $GuestEmail,
+        $RedirectUrl,
+        $SendInviteMessage,
+        $GuestDisplayName
+    )
+    $inviteurl = $groupurl = "https://graph.microsoft.com/v1.0/invitations"
+
+    $inviteurl = $groupurl = "https://graph.microsoft.com/v1.0/invitations"
+    $newinvitebody = @{
+    "invitedUserEmailAddress" = $GuestEmail;
+    "inviteRedirectUrl"=$GuestEmail;
+    "sendInvitationMessage"=$SendInviteMessage;
+    "invitedUserDisplayName"=$GuestDisplayName
+    }
+$newinvitebody = $newinvitebody | ConvertTo-Json -Depth 10
+Invoke-RestMethod -Uri $inviteurl -Method POST -Headers @{Authorization = $authentication.result.CreateAuthorizationHeader()} -ContentType application/json -Body $newinvitebody
+
 }
